ee10bab78f1d4cdfaf0b095c5cf51f94c2af6e7c
Wesley Oliveira <145072447+woliveiradev@users.noreply.github.com>
2023-11-14 19:06:35 -0300

    refactor: extends DomainEvent of Bus Event

--- src/core/domain/event.ts
+++ src/core/domain/event.ts
@@ -1,22 +1,15 @@
-import { createHash, randomUUID } from 'crypto';
-import { DomainEventMessage, EventMetadata, EventType } from './types';
+import { DomainEventMessage } from './types';
+import { EventCorrelationId, EventTopic } from 'core/bus/event/types';
+import { Event } from 'core/bus';
 
-export abstract class DomainEvent<Message = object> {
-  public readonly eventType: EventType;
-  public readonly message: Message;
-  public readonly metadata: EventMetadata;
-
-  protected constructor(
-    eventType: EventType,
+export abstract class DomainEvent<Message = unknown> extends Event<
+  Message & DomainEventMessage
+> {
+  constructor(
+    topic: EventTopic,
     message: Message & DomainEventMessage,
+    correlationId?: EventCorrelationId,
   ) {
-    this.eventType = eventType;
-    this.message = message;
-    const timestamp = new Date();
-    const key = `${randomUUID()}.${eventType}.${timestamp}`;
-    this.metadata = {
-      signatureKey: createHash('md5').update(key).digest('hex'),
-      timestamp,
-    };
+    super(topic, message, correlationId);
   }
 }
