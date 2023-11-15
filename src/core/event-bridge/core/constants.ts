ddad19f4680e18b1a2099524f630e33b233daa7e
Wesley Oliveira <145072447+woliveiradev@users.noreply.github.com>
2023-11-15 02:17:13 -0300

    refactor: move topic regex to constants file

---
+++ src/core/event-bridge/core/constants.ts
@@ -0,0 +1,1 @@
+export const topicRules = /^[A-Z][a-zA-Z]+\.[A-Z][a-zA-Z]+$|^[a-zA-Z]+\.\*$/;
--- src/core/event-bridge/core/core-proxy.bridge.ts
+++ src/core/event-bridge/core/core-proxy.bridge.ts
@@ -1,4 +1,5 @@
 import { Event } from '../event/event.bridge';
+import { topicRules } from './constants';
 import { InvalidTopicFormat } from './exceptions/invalid-topic-format.exception';
 import { TopicNotRegistered } from './exceptions/topic-not-registered.exception';
 import { EventAction, EventActionCondition, EventBridge } from './types';
@@ -12,10 +13,9 @@ export class BridgeCoreProxy implements EventBridge {
     condition?: EventActionCondition,
   ): void {
     /*
-      A topic will only be accepted if it follows the "Planet.EarthPlanet" or "Planet.*" pattern.
-      Words follow the Pascal Case pattern.
+      A topic will only be accepted if it follows the "Planet.EarthPlanet"
+      or "Planet.*" pattern. Words follow the Pascal Case pattern.
     */
-    const topicRules = /^[A-Z][a-zA-Z]+\.[A-Z][a-zA-Z]+$|^[a-zA-Z]+\.\*$/;
     if (!topicRules.test(topic)) throw new InvalidTopicFormat();
     this.bridgeCore.register(topic, action, condition);
   }
