731380a45bcaca71fd1c67967ebfa06b137d7663
Wesley Oliveira <145072447+woliveiradev@users.noreply.github.com>
2023-11-20 08:15:25 -0300

    feat: add cors and helmet

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
+import helmet from '@fastify/helmet';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { HttpStatus } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { RootModule } from './module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(RootModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(
+    RootModule,
+    new FastifyAdapter(),
+  );
+  app.enableCors({
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: HttpStatus.NO_CONTENT,
+  });
+  app.register(helmet);
   await app.listen(3333);
 }
 bootstrap();
