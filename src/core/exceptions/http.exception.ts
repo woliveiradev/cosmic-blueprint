7235cecb76a99c0fb47a4c1a5d401e4e76e2753b
Wesley Oliveira <145072447+woliveiradev@users.noreply.github.com>
2023-11-20 07:44:28 -0300

    feat: add log error in exception filter

--- src/core/exceptions/http.exception.ts
+++ src/core/exceptions/http.exception.ts
@@ -4,15 +4,20 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  Inject,
 } from '@nestjs/common';
+import { LOGGER_TOKEN, Logger } from 'core/logger';
 
 @Catch(HttpException)
 export class HttpExceptionHandler implements ExceptionFilter {
+  constructor(@Inject(LOGGER_TOKEN) private readonly logger: Logger) {}
+
   catch(exception: HttpException, host: ArgumentsHost): void {
     const context = host.switchToHttp();
     const request = context.getRequest<FastifyRequest>();
     const response = context.getResponse<FastifyReply>();
     const status = exception.getStatus();
+    this.logger.error('Request interrupted with exception', exception.stack);
     response.status(status).send({
       code: exception.name,
       message: exception.message,
